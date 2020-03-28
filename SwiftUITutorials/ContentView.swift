7a860a4e467c6a8b42e3fd9cedacde9497fc7f5b
Brian Michael <clay.mrx10@gmail.com>
2020-03-28 15:18:25 +0000

    Initial Commit

---
+++ SwiftUITutorials/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITutorials
+//
+//  Created by Brian Hashirama on 3/28/20.
+//  Copyright © 2020 PROIT-CONSULTING. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
