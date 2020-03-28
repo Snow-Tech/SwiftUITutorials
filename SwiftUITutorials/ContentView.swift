88efb5c5151d0519cf744dc6aa51729cbc5896d5
Brian Michael <clay.mrx10@gmail.com>
2020-03-28 16:37:50 +0000

    Components and Visual Effects

--- SwiftUITutorials/ContentView.swift
+++ SwiftUITutorials/ContentView.swift
@@ -11,43 +11,39 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        
         ZStack {
-            VStack {
-                Spacer()
-            }
-            .frame(width: 300, height: 220) // a dimensao da stack principal (VStack)
-            .background(Color.blue) // cor do fundo
-            .cornerRadius(20) // bordas
-            .shadow(radius: 20) //sombra
-            .offset(x: 0, y: -20) // move na horizontal(x) ou na vertical(y)
             
-            VStack {
-                HStack {
-                    VStack(alignment: .leading) {  // alinhamento da stack (leading, trailing, center), o conteudo dentro da stack se fica na esquerda, ou direita, ou no centralizado
-                        Text("iOS Engineer Card")
-                            .font(.title) // fonte
-                            .fontWeight(.semibold) // tipo de fonte (bold, semibold, italic)
-                            .foregroundColor(.white) // cor do texto
-                        Text("Brian Michael")
-                            .foregroundColor(.white) // cor do texto
-                    }
-                    Spacer() // poe o maximo de espaço que ocupa todo comprimento
-                    Image("Logo1") // imagem
-                }
-                    // .padding() // espaço para todos os lados
-                    .padding(.horizontal, 20) // espaço apenas no lado esquerdo e direito (HStack)
-                    .padding(.top, 20) // espaço apenas para cima (Hstack)
-                Spacer()
-                Image("Card1")
-                    .resizable() // adpta a imagem para qualquer dimensao dada
-                    .aspectRatio(contentMode: .fill) // proporção da imagem se é fit ou fill
-                    .frame(width: 300, height: 110, alignment: .top) // dimensao para a imagem
-            }
-                .frame(width: 340, height: 200) // a dimensao da stack principal (VStack)
-                .background(Color.black) // cor do fundo
+            TitleSubView()
+                .blur(radius: 20) // efeito blur (quanto menor o valor menos desfocado será)
+            
+            BackCardView()
+                .background(Color("card4")) // cor do fundo
                 .cornerRadius(20) // bordas
                 .shadow(radius: 20) //sombra
+                .offset(x: 0, y: -40) // move na horizontal(x) ou na vertical(y)
+                .scaleEffect(0.9) // da um efeito tipo escala
+                .rotationEffect(.degrees(10)) // da efeito de rotacao
+                .rotation3DEffect(Angle(degrees: 10), axis: (x: 10, y: 0, z: 0)) // da efeito de rotacao 3d e defimos em que eixo queremos esse efeito (x,y,z)
+                .blendMode(.hardLight) // poe o conteudo meio opaco ou invisivel (modo de mistura)
+            
+            BackCardView()
+                .background(Color("card3")) // cor do fundo
+                .cornerRadius(20) // bordas
+                .shadow(radius: 20) //sombra
+                .offset(x: 0, y: -20) // move na horizontal(x) ou na vertical(y)
+                .scaleEffect(0.95) // da um efeito tipo escala
+                .rotationEffect(.degrees(5)) // da efeito de rotacao
+                .rotation3DEffect(Angle(degrees: 5), axis: (x: 10, y: 0, z: 0)) // da efeito de rotacao 3d e defimos em que eixo queremos esse efeito (x,y,z)
+                .blendMode(.hardLight) // poe o conteudo meio opaco ou invisivel (modo de mistura)
+            
+            
+            CardView()
+                .blendMode(.hardLight) // poe o conteudo meio opaco ou invisivel (modo de mistura)
+            
+            
+            
+            BottomCardView()
+                .blur(radius: 20)  // efeito blur (quanto menor o valor menos desfocado será)
         }
         
     }
@@ -56,5 +52,100 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+// foi criado uma subview ou componet (Extraxt Subview)
+struct CardView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {  // alinhamento da stack (leading, trailing, center), o conteudo dentro da stack se fica na esquerda, ou direita, ou no centralizado
+                    Text("iOS Engineer Card")
+                        .font(.title) // fonte
+                        .fontWeight(.semibold) // tipo de fonte (bold, semibold, italic)
+                        .foregroundColor(.white) // cor do texto
+                    Text("Brian Michael")
+                        .foregroundColor(.white) // cor do texto
+                }
+                Spacer() // poe o maximo de espaço que ocupa todo comprimento
+                Image("Logo1") // imagem
+            }
+                // .padding() // espaço para todos os lados
+                .padding(.horizontal, 20) // espaço apenas no lado esquerdo e direito (HStack)
+                .padding(.top, 20) // espaço apenas para cima (Hstack)
+            Spacer()
+            Image("Card1")
+                .resizable() // adpta a imagem para qualquer dimensao dada
+                .aspectRatio(contentMode: .fill) // proporção da imagem se é fit ou fill
+                .frame(width: 300, height: 110, alignment: .top) // dimensao para a imagem
+        }
+            .frame(width: 340, height: 200) // a dimensao da stack principal (VStack)
+            .background(Color.black) // cor do fundo
+            .cornerRadius(20) // bordas
+            .shadow(radius: 20) //sombra
+    }
+}
+
+
+//outra subview ou component que são os outros cartões
+struct BackCardView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+        }
+            .frame(width: 340, height: 220) // a dimensao da stack principal (VStack)
+    }
+}
+
+
+//outra subview ou component que contem o TITULO E A IMAGEM
+struct TitleSubView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Cartões") // texto
+                    .font(.largeTitle) // fonte
+                    .fontWeight(.bold) // tipo de fonte
+                Spacer() // da o espaco maximo na horizontal pk está em uma HStack
+            }
+                .padding()  // espaco nos quadro lados
+            Image("Background1") // imagem
+            Spacer() // este espaço é dado na vertical para ocupar a altura toda do dispositivo
+        }
+    }
+}
+
+
+
+
+//outra subview ou component que é o menu que está por baixo
+struct BottomCardView: View {
+    var body: some View {
+        VStack(spacing: 20) {  // dá um espaço na VStack
+            
+            
+            Rectangle() // cria um rectangulo
+                .frame(width: 40, height: 5) // dimensoes do rectangulo
+                .cornerRadius(3) // bordas
+                .opacity(0.1) // opacidade
+            
+            
+            Text("Do not be anxious about tomorrow, for tomorrow will be anxious for itself. Let the day's own trouble be sufficient for the day.") // texto
+                .font(.subheadline) // fonte
+                .fontWeight(.medium) // tipo de fonte
+                .multilineTextAlignment(.center) // alinhamento do texto (centro, esquerda, direita)
+                .lineSpacing(4) // espaço entre o texto
+            
+            Spacer()
+        }
+            .padding(.top, 8) // espaço para cima
+            .padding(.horizontal, 20) // espaço horizontal
+            .frame(maxWidth: .infinity) // da o maximo de comprimeto
+            .background(Color.white) // cor do fundo
+            .cornerRadius(30) // bordas
+            .shadow(radius: 20) // sombra
+            .offset(x: 0, y: 500) // move na horizontal(x) ou na vertical(y)
     }
 }
