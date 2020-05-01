7ba907598fa632507aacf5d5c1532de0e7c33607
Brian Michael <clay.mrx10@gmail.com>
2020-05-01 20:27:25 +0100

    Z Index and Status Bar

--- SwiftUITutorials/CourseList.swift
+++ SwiftUITutorials/CourseList.swift
@@ -9,12 +9,48 @@
 import SwiftUI
 
 struct CourseList: View {
+    
+
+    @State var courses = courseData
+    @State var active = false
+    
     var body: some View {
         
-        VStack {
-            CourseView()
+        ZStack {
+            
+            Color.black.opacity(active ? 0.5 : 0)
+                .animation(.linear)
+                .edgesIgnoringSafeArea(.all)
+            
+            
+            
+            ScrollView {
+                VStack(spacing: 30) {
+                    
+                    Text("Criadores")
+                        .font(.largeTitle).bold()
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 30)
+                        .padding(.top, 20)
+                        .blur(radius: active ? 20 : 0)
+                    
+                    ForEach(courses.indices, id: \.self) { index in
+                        GeometryReader { geometry in
+                            CourseView(show: self.$courses[index].show, course: self.courses[index], active: self.$active)
+                                .offset(y: self.courses[index].show ? -geometry.frame(in: .global).minY : 0)
+                        }
+                        .frame(height: 280)
+                        .frame(maxWidth: self.courses[index].show ? .infinity : screen.width - 60 )
+                        .zIndex(self.courses[index].show ? 1 : 0) // quando clica ele preenche o ecra e deixa o resto por trás, tenta comentar esta linha e ve a diferença
+                    }
+                }
+                .frame(width: screen.width)
+                .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+            }
+            
+            .statusBar(hidden: active ? true : false) // faz desparecer o status bar
+                .animation(.linear)
         }
-        
         
     }
 }
@@ -29,10 +65,13 @@ struct CourseList_Previews: PreviewProvider {
 
 struct CourseView: View {
     
-    @State var show = false
-    
+    @Binding var show: Bool
+    var course: Course
+    @Binding var active: Bool
     
     var body: some View {
+        
+        // Conteudo do cartão
         ZStack(alignment: .top) {
             VStack(alignment: .leading, spacing: 30) {
                 Text("When you don't create things, you become defined by your tastes rather than ability. your tastes only narrow & exclude people. so create.")
@@ -56,17 +95,17 @@ struct CourseView: View {
                 // TEXTO
                 HStack(alignment:.top) {
                     VStack(alignment: .leading, spacing: 8) {
-                        Text("Brian Michael")
+                        Text(course.title)
                             .font(.system(size: 24, weight: .bold))
                             .foregroundColor(.white)
-                        Text("20 Sections")
+                        Text(course.subtitle)
                             .foregroundColor(Color.white.opacity(0.7))
                     }
                     
                     //LOGO
                     Spacer()
                     ZStack {
-                        Image(uiImage: #imageLiteral(resourceName: "Logo1"))
+                        Image(uiImage: course.logo)
                             .opacity(show ? 0 : 1)
                         VStack {
                             Image(systemName: "xmark")
@@ -84,7 +123,7 @@ struct CourseView: View {
                 
                 //IMAGEM DO CARTÃO
                 Spacer()
-                Image(uiImage: #imageLiteral(resourceName: "Card2"))
+                Image(uiImage: course.image)
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .frame(maxWidth:. infinity)
@@ -95,14 +134,38 @@ struct CourseView: View {
             .padding(.top, show ? 30: 0)
                 //.frame(width: show ? screen.width : screen.width - 60  , height: show ? screen.height : 280)
                 .frame(maxWidth: show ? .infinity : screen.width - 60  , maxHeight: show ? 460 : 280)
-                .background(Color(#colorLiteral(red: 0.5725490451, green: 0, blue: 0.2313725501, alpha: 1)))
+                .background(Color(course.color))
                 .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
-                .shadow(color: Color(#colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)).opacity(0.3), radius: 20, x: 0, y: 20)
+                .shadow(color: Color(course.color).opacity(0.3), radius: 20, x: 0, y: 20)
                 .onTapGesture {
                     self.show.toggle()
+                    self.active.toggle()
             }
         }
+        .frame(height: show ? screen.height : 280)
         .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
         .edgesIgnoringSafeArea(.all)
     }
 }
+
+
+
+struct Course: Identifiable {
+    var id = UUID()
+    var title: String
+    var subtitle: String
+    var image: UIImage
+    var logo: UIImage
+    var color: UIColor
+    var show: Bool
+}
+
+
+var courseData = [
+        Course(title: "Steve Jobs", subtitle: "Apple", image: #imageLiteral(resourceName: "Card4"), logo: #imageLiteral(resourceName: "Logo1"), color: #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1), show: false),
+        Course(title: "Bill Gates", subtitle: "Microsoft", image: #imageLiteral(resourceName: "Card5"), logo: #imageLiteral(resourceName: "Logo1"), color: #colorLiteral(red: 0.5725490451, green: 0, blue: 0.2313725501, alpha: 1), show: false),
+        Course(title: "Linus Torvalds", subtitle: "Linux", image: #imageLiteral(resourceName: "Card3"), logo: #imageLiteral(resourceName: "Logo1"), color: #colorLiteral(red: 0.1215686277, green: 0.01176470611, blue: 0.4235294163, alpha: 1), show: false),
+        Course(title: "Brian Michael", subtitle: "Izanami", image: #imageLiteral(resourceName: "Card1"), logo: #imageLiteral(resourceName: "Logo1"), color: #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1), show: false)
+
+
+]
